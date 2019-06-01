95cfa55720bdb72f21d3456b9a649fe7d6eb6f37
qamilnowak <qamilnowak@gmail.com>
2019-06-01 13:23:59 +0200

    merge fix

--- kalkulator.py
+++ kalkulator.py
@@ -1,13 +1,11 @@
 def kurczaki():
-<<<<<<< HEAD
     print('kurczaki')
 
 def nowa_funkca():
     print('ziemniaki2')
-=======
-    print('ziemniaki')
 def ziemniaki():
     print('ziemniaki')
 def liczba_ziemniakow():
     print('liczba_ziemniakow')
->>>>>>> master
+
+kurczaki()
